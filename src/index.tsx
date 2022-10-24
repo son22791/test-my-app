1be19f0c5f3c47c309d6814bf874b13fd36d149d
wize-tien.hoang <tien.hoang@wize-net.co.jp>
2022-10-24 08:47:24 +0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from "web3";
 
+
+const getLibrary = (provider:any): Web3 => {
+  return provider
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+      </Web3ReactProvider>
   </React.StrictMode>
 );
 
